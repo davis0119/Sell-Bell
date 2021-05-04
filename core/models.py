2b556a893b752fa64dc1db9f53d49fa1b00bdcc3
Davis Tran <davistran0119@gmail.com>
2021-05-03 22:57:02 -0400

    added documentation

--- core/models.py
+++ core/models.py
@@ -1,8 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User 
 
-# each has a form, send a postID along with the comment?
-
 # Bells (services)
 class Bell(models.Model):
     title = models.CharField(max_length=50)
